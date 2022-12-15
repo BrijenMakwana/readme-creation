23467fd20da29c72bb24f58346efb2be28d01609
Brijen Makwana <brijenma@gmail.com>
2022-12-15 11:16:48 +0530

    code

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import "./HomePage.css";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { nord as codeStyle } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 export default function HomePage() {
   const [text, setText] = useState("");
@@ -23,6 +25,24 @@ export default function HomePage() {
           children={text}
           value={text}
           remarkPlugins={[remarkGfm]}
+          components={{
+            code({ node, inline, className, children, ...props }) {
+              const match = /language-(\w+)/.exec(className || "");
+              return !inline && match ? (
+                <SyntaxHighlighter
+                  children={String(children).replace(/\n$/, "")}
+                  style={codeStyle}
+                  language={match[1]}
+                  PreTag="div"
+                  {...props}
+                />
+              ) : (
+                <code className={className} {...props}>
+                  {children}
+                </code>
+              );
+            },
+          }}
         >
           {text}
         </ReactMarkdown>
